e1fb761824baeb427f614258c382d10e875fd14a
Virtuos Digital <shaurydeepsaxena@gmail.com>
2024-03-05 19:13:41 +0530

    loffupdate

--- drf/mysite/movies/models.py
+++ drf/mysite/movies/models.py
@@ -10,6 +10,7 @@ class Moviedata(models.Model):
     name=models.CharField(max_length=200)
     duration=models.FloatField()
     rating=models.FloatField()
-
+    cat=models.CharField(max_length=200, default='action')
+    image=models.ImageField(upload_to='Images/',default="Images/None/img.jpg")
 
 
--- drf/mysite/mysite/urls.py
+++ drf/mysite/mysite/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework import routers
+from movies.views import MovieViewSet, ActionViewSet, ComedyViewSet
+from django.conf.urls.static import static
+from django.conf import settings
+
+router = routers.SimpleRouter()
+router.register('movies',MovieViewSet)
+router.register('action',ActionViewSet)
+router.register('comedy',ComedyViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',include(router.urls)),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
