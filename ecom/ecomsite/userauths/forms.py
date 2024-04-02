edfac0215ff32300c41c852613bcecde42d785ca
Virtuos Digital <shaurydeepsaxena@gmail.com>
2024-04-02 18:59:03 +0530

    register form url setup loff

---
+++ ecom/ecomsite/userauths/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from userauths.models import User
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model=User
+        fields=['username','email']
--- ecom/ecomsite/userauths/views.py
+++ ecom/ecomsite/userauths/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from userauths.forms import UserRegistrationForm
 
 # Create your views here.
+def register(request):
+
+    if(request.method=="POST"):
+        print("User registered")
+
+    else:
+        print("User not registered")
+
+    form=UserRegistrationForm()
+
+    context={
+        'form':form,
+    }
+    
+
+    return render(request,'userauths/register.html',context)
